52ce8c7bf7337bcf32912bc25828fab8f2868359
Barchinoy0803 <barchinoyyusupova33@gmail.com>
2025-06-21 13:35:06 +0500

    template updated

--- src/pages/Templates/index.tsx
+++ src/pages/Templates/index.tsx
@@ -8,7 +8,6 @@ import { FaRegTrashCan } from "react-icons/fa6";
 import { FaEdit, FaPlus } from 'react-icons/fa';
 import { useNavigate } from 'react-router-dom';
 import CustomTabs from '../../components/Tabs';
-import { useTranslator } from '../../hooks/useTranslator';
 
 const Templates = () => {
   const navigate = useNavigate()
@@ -16,7 +15,6 @@ const Templates = () => {
   const [activeTab, setActiveTab] = useState<string>("all")
   const [selectedIds, setSelectedIds] = useState<GridRowSelectionModel>();
 
-  const {t: buttons} = useTranslator()
   const { data: allData } = useGetTemplatesQuery({})
   const [deleteTemplate, { isLoading: deleteLoading }] = useDeleteTemplateMutation()
   const { data, isLoading } = useGetAllUserTemplatesQuery({})
@@ -26,8 +24,11 @@ const Templates = () => {
     toast.success("User deleted!")
   }
 
+  const ids = useMemo(() => {
+    return selectedIds?.ids ? [...selectedIds.ids] : []
+  }, [selectedIds])
+
   const handleUpdate = () => {
-    const ids = [...selectedIds?.ids!]
     if (ids.length > 1) {
       toast.error("You cannot update more than one template at the same time")
     } else {
@@ -38,6 +39,7 @@ const Templates = () => {
   const handleCreate = () => {
     navigate(`/dashboard/template/new`)
   }
+
   const isAllTemplates = useMemo(() => {
     return activeTab === "all"
   }, [activeTab])
@@ -45,15 +47,25 @@ const Templates = () => {
   return (
     <div className='container mx-auto flex flex-col gap-3 mt-[50px] mb-[30px]'>
       <div className='flex items-center gap-5 mb-3'>
-        <Tooltip placement='top' title='Delete templates'>
-          <Button color='error' disabled={deleteLoading || isAllTemplates} onClick={handleDelete} startIcon={<FaRegTrashCan />} variant='outlined'>Delete</Button>
-        </Tooltip>
-        <Tooltip placement='top' title='Update templates'>
-          <Button disabled={deleteLoading || isAllTemplates} onClick={handleUpdate} startIcon={<FaEdit />} variant='outlined'>Update</Button>
-        </Tooltip>
-        <Tooltip placement='top' title='Create templates'>
-          <Button disabled={deleteLoading} onClick={handleCreate} startIcon={<FaPlus />} variant='outlined'>Create</Button>
-        </Tooltip>
+        {
+          isAllTemplates ?
+            <Tooltip placement='top' title='Please select only one template to field out!'>
+              <span>
+                <Button disabled={ids.length !== 1}>Start Survey</Button>
+              </span>
+            </Tooltip>
+            : <>
+              <Tooltip placement='top' title='Delete templates'>
+                <Button color='error' disabled={deleteLoading || isAllTemplates} onClick={handleDelete} startIcon={<FaRegTrashCan />} variant='outlined'>Delete</Button>
+              </Tooltip>
+              <Tooltip placement='top' title='Update templates'>
+                <Button disabled={deleteLoading || isAllTemplates} onClick={handleUpdate} startIcon={<FaEdit />} variant='outlined'>Update</Button>
+              </Tooltip>
+              <Tooltip placement='top' title='Create templates'>
+                <Button disabled={deleteLoading} onClick={handleCreate} startIcon={<FaPlus />} variant='outlined'>Create</Button>
+              </Tooltip>
+            </>
+        }
       </div>
       {
         isLoading ? <CircularProgress /> : <CustomTabs setActiveTab={setActiveTab} activeTab={activeTab} allData={allData} columns={TemplateTableColumns} selectedIds={selectedIds} setSelectedIds={setSelectedIds} data={data} />
