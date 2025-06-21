a6af441b61703fe62ded8cffeab65d0debbd27e6
Barchinoy0803 <yusupovabarchinoy33@gmail.com>
2025-06-21 13:56:15 +0500

    sending request logic fixed

--- src/service/api/template.api.ts
+++ src/service/api/template.api.ts
@@ -1,66 +1,83 @@
 import { mainApi } from "./api";
 
 const extendedApi = mainApi.injectEndpoints({
-    endpoints: (build) => ({
-        getTemplates: build.query({
-            query: (params) => ({
-                method: "GET",
-                url: "/template",
-                params
-            }),
-            providesTags: ['TEMPLATE']
-        }),
-        getOneTemplate: build.query({
-           query: (id) => ({
-            method: "GET",
-            url: `/template/${id}`,
-            id
-           }) 
-        }),
-        getAllUserTemplates: build.query({
-            query: (params) => ({
-                method: "GET",
-                url: "/template/owner",
-                params
-            }),
-            providesTags: ['TEMPLATE']
-        }),
-        createTemplate: build.mutation({
-            query: (body) => ({
-                method: "POST",
-                url: "/template",
-                body
-            }),
-            invalidatesTags: ['TEMPLATE']
-        }),
-        deleteTemplate: build.mutation({
-            query: (body) => ({
-                method: "DELETE",
-                url: "/template",
-                body
-            }),
-            invalidatesTags: ['TEMPLATE']
-        }),
-        updateTemplate: build.mutation({
-            query: ({ id, body }) => ({
-                method: "PATCH",
-                url: `/template/${id}`,
-                body
-            }),
-            invalidatesTags: ['TEMPLATE']
-        }),
-        fileUpload: build.mutation({
-            query: ({ body }) => ({
-                method: "POST",
-                url: `/file-upload`,
-                body,
-                headers: {
-                    'X-skip-Content-Type': 'true'
-                }
-            }),
-        })
+  endpoints: (build) => ({
+    getTemplates: build.query({
+      query: (params) => ({
+        method: "GET",
+        url: "/template",
+        params
+      }),
+      providesTags: [{ type: 'TEMPLATE', id: 'LIST' }],
     }),
-    overrideExisting: false
-})
 
-export const { useGetTemplatesQuery, useCreateTemplateMutation, useDeleteTemplateMutation, useGetAllUserTemplatesQuery, useUpdateTemplateMutation, useFileUploadMutation, useGetOneTemplateQuery } = extendedApi
+    getAllUserTemplates: build.query({
+      query: (params) => ({
+        method: "GET",
+        url: "/template/owner",
+        params
+      }),
+      providesTags: [{ type: 'TEMPLATE', id: 'LIST' }],
+    }),
+
+    getOneTemplate: build.query({
+      query: (id) => ({
+        method: "GET",
+        url: `/template/${id}`,
+      }),
+      providesTags: (result, error, id) => [{ type: 'TEMPLATE', id }],
+    }),
+
+    createTemplate: build.mutation({
+      query: (body) => ({
+        method: "POST",
+        url: "/template",
+        body
+      }),
+      invalidatesTags: [{ type: 'TEMPLATE', id: 'LIST' }],
+    }),
+
+    deleteTemplate: build.mutation({
+      query: (body) => ({
+        method: "DELETE",
+        url: "/template",
+        body
+      }),
+      invalidatesTags: [{ type: 'TEMPLATE', id: 'LIST' }],
+    }),
+
+    updateTemplate: build.mutation({
+      query: ({ id, body }) => ({
+        method: "PATCH",
+        url: `/template/${id}`,
+        body
+      }),
+      invalidatesTags: ({ id }) => [
+        { type: 'TEMPLATE', id },
+        { type: 'TEMPLATE', id: 'LIST' }
+      ],
+    }),
+
+    fileUpload: build.mutation({
+      query: ({ body }) => ({
+        method: "POST",
+        url: `/file-upload`,
+        body,
+        headers: {
+          'X-skip-Content-Type': 'true'
+        }
+      }),
+    }),
+  }),
+  overrideExisting: false
+});
+
+export const {
+  useGetTemplatesQuery,
+  useCreateTemplateMutation,
+  useDeleteTemplateMutation,
+  useGetAllUserTemplatesQuery,
+  useUpdateTemplateMutation,
+  useFileUploadMutation,
+  useGetOneTemplateQuery
+} = extendedApi;
