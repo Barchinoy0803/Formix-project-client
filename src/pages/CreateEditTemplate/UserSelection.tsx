1dff6ff0bef1898e4d82faa720a98fff624cc346
Barchinoy0803 <yusupovabarchinoy33@gmail.com>
2025-06-28 17:55:35 +0500

    autocomplete fixed

--- src/pages/CreateEditTemplate/UserSelection.tsx
+++ src/pages/CreateEditTemplate/UserSelection.tsx
@@ -1,65 +1,66 @@
-import { memo } from 'react'
-import { Autocomplete, CircularProgress, TextField } from '@mui/material'
-import { Control, Controller } from 'react-hook-form'
-import { getAllUserOptions } from './helpers'
-import { useGetUsersQuery } from '../../service/api/user.api'
-import { TemplateForm, AllowedUsers } from '../../types/form'
+import { memo, useMemo } from 'react';
+import { Autocomplete, CircularProgress, TextField } from '@mui/material';
+import { Control, Controller } from 'react-hook-form';
+import { useGetUsersQuery } from '../../service/api/user.api';
+import { TemplateForm, AllowedUsers } from '../../types/form';
+
+type UserOption = AllowedUsers;
+
+function isAllowedUser(item: unknown): item is AllowedUsers {
+  return !!item && typeof item === 'object' && 'id' in item && 'username' in item;
+}
 
 interface UserSelectionProps {
-    name: keyof TemplateForm,
-    control: Control<TemplateForm>,
+  name: keyof TemplateForm;         
+  control: Control<TemplateForm>;
 }
 
 const UserSelection = ({ control, name }: UserSelectionProps) => {
-    const { data: allUsers } = useGetUsersQuery({})
-    const userOptions = getAllUserOptions(allUsers)
+  const { data: allUsers, isFetching } = useGetUsersQuery({});
 
-    return (
-        <>
-            {
-                allUsers ? <div>
-                    <Controller
-                        control={control}
-                        name={name}
-                        render={({ field: { onChange, value }, fieldState: { error } }) => {
-                            const usersArray = Array.isArray(value) &&
-                                value.every(item => typeof item === 'object' && 'id' in item)
-                                ? value as AllowedUsers[]
-                                : []
+  const userOptions: UserOption[] = useMemo(
+    () =>
+      (allUsers ?? []).map((u: { id: any; username: any; }) => ({
+        id: u.id,
+        username: u.username,
+      })),
+    [allUsers]
+  );
 
-                            return (
-                                <Autocomplete
-                                    className="w-full"
-                                    multiple
-                                    limitTags={6}
-                                    id="multiple-limit-tags"
-                                    options={userOptions}
-                                    value={usersArray.map(user => user.id) || []}
-                                    onChange={(_, newValue) => {
-                                        const selectedUsers = newValue.map(id => ({
-                                            id,
-                                            username: userOptions.find((opt: any) => opt.value === id)?.label || id
-                                        }))
-                                        onChange(selectedUsers)
-                                    }}
-                                    renderInput={(params) => (
-                                        <TextField
-                                            {...params}
-                                            label="Users"
-                                            placeholder="Users"
-                                            error={!!error}
-                                            helperText={error?.message}
-                                        />
-                                    )}
-                                />
-                            )
-                        }}
-                    />
-                </div> : <CircularProgress />
-            }
-        </>
+  if (isFetching) return <CircularProgress />;
 
-    )
-}
+  return (
+    <Controller
+      control={control}
+      name={name}
+      render={({ field: { onChange, value }, fieldState: { error } }) => {
+        const usersArray: AllowedUsers[] =
+          Array.isArray(value) && value.every(isAllowedUser) ? value : [];
 
-export default memo(UserSelection)
+        return (
+          <Autocomplete
+            className="w-full"
+            multiple
+            limitTags={6}
+            options={userOptions}
+            value={usersArray}
+            getOptionLabel={(option) => option.username}
+            isOptionEqualToValue={(opt, val) => opt.id === val.id}
+            onChange={(_, newValue) => onChange(newValue)}
+            renderInput={(params) => (
+              <TextField
+                {...params}
+                label="Users"
+                placeholder="Users"
+                error={!!error}
+                helperText={error?.message}
+              />
+            )}
+          />
+        );
+      }}
+    />
+  );
+};
+
+export default memo(UserSelection);
--- src/types/form.ts
+++ src/types/form.ts
@@ -36,6 +36,7 @@ export interface TemplateForm {
 }
 
 export interface AllowedUsers {
+    username: string;
     id: string;
 }
 
