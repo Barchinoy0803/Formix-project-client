dc6d5efbb4b0725893408271b4c6a42b2af37c7d
Barchinoy0803 <barchinoyyusupova33@gmail.com>
2025-07-04 11:48:15 +0500

    some updates with overal style

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -8,7 +8,7 @@ import { setSearchText } from "../../redux/features/template.slice";
 
 const Navbar = () => {
   const { t } = useTranslator('auth')
-  const { t:dashboard } = useTranslator('dashboard')
+  const { t: dashboard } = useTranslator('dashboard')
   const dispatch = useDispatch()
   const [search, setSearch] = useState<string>("")
 
@@ -21,17 +21,24 @@ const Navbar = () => {
     <nav className="bg-white shadow-sm border-b border-gray-100 mb-6">
       <div className="max-w-7xl mx-auto">
         <div className="flex justify-between items-center h-16">
-          <div className="flex-shrink-0">
-            <span className="text-[24px] font-bold text-[#47aed6]">Formify</span>
+
+          <div className="flex items-center justify-center p-8">
+            <div className="font-sans text-[28px] font-normal tracking-wider text-cyan-400 relative py-3">
+              <span className="font-bold text-cyan-300 text-3xl mr-px relative">F</span>
+              <span className="font-light tracking-wide">ORMIX</span>
+              <div className="absolute bottom-0 left-0 w-6 h-1 bg-gradient-to-r from-cyan-300 to-cyan-400"></div>
+              <div className="absolute top-0 right-0 w-6 h-px bg-cyan-400 opacity-50"></div>
+            </div>
           </div>
+
+
           <div>
             <div className="flex gap-1 items-center justify-center bg-[#f1f3f4] rounded-3xl">
               <CiSearch className="mx-3 text-[20px]" />
               <input value={search} onChange={(e) => setSearch(e.target.value)} className="w-[600px] h-[45px] outline-none" placeholder={dashboard('search')} />
             </div>
           </div>
-          <div className="flex gap-1">
-            <LanguageSwitcher />
+          <div className="flex gap-2">
             <NavLink
               to="auth/register"
               className={({ isActive }) =>
@@ -54,6 +61,7 @@ const Navbar = () => {
             >
               {t('login')}
             </NavLink>
+            <LanguageSwitcher />
           </div>
         </div>
       </div>
